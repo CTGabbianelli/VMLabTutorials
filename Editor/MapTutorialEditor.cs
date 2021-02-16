eeec40d76485ff7c782e77f684d1720ad134483d
CGabbianelli <50253510+CGabbianelli@users.noreply.github.com>
2021-02-16 14:26:33 -0500

    Added Assembly References

---
+++ Editor/MapTutorialEditor.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using TMPro;
+
+public class MapTutorialEditor : EditorWindow
+{
+    string informationString;
+    string titleString;
+    string buttonTitleString;
+    TutorialMapScriptableObject tutorialMapObject;
+    RectTransform tutorialMapButtonTransform;
+    RectTransform maskTransform;
+    RectTransform panelTransform;
+    RectTransform triangleTransform;
+    [SerializeField]
+    TMP_Text informationText;
+    [SerializeField]
+    TMP_Text titleText;
+    [SerializeField]
+    TMP_Text buttonTitleText;
+    [MenuItem("Tools/MapTutorialEditor")]
+
+    public static void ShowMapWindow()
+    {
+        GetWindow(typeof(MapTutorialEditor));
+    }
+    private void OnGUI()
+    {
+        GUILayout.Label("Save Tutorial Preset", EditorStyles.boldLabel);
+        tutorialMapObject = EditorGUILayout.ObjectField("Tutorial Scriptable Object", tutorialMapObject, typeof(TutorialMapScriptableObject), false) as TutorialMapScriptableObject;
+        tutorialMapButtonTransform = EditorGUILayout.ObjectField("Map Button Rect Transform", tutorialMapButtonTransform, typeof(RectTransform), true) as RectTransform;
+        maskTransform = EditorGUILayout.ObjectField("Mask Rect Transform", maskTransform, typeof(RectTransform), true) as RectTransform;
+        panelTransform = EditorGUILayout.ObjectField("Panel Rect Transform", panelTransform, typeof(RectTransform), true) as RectTransform;
+        triangleTransform = EditorGUILayout.ObjectField("Triangle Rect Transform", triangleTransform, typeof(RectTransform), true) as RectTransform;
+        titleString = EditorGUILayout.TextField("Title", titleString);
+        titleText = EditorGUILayout.ObjectField("Title Text", titleText, typeof(TMP_Text), true) as TMP_Text;
+        informationString = EditorGUILayout.TextField("Information", informationString);
+        informationText = EditorGUILayout.ObjectField("Information Text", informationText, typeof(TMP_Text), true) as TMP_Text;
+        buttonTitleString = EditorGUILayout.TextField("Button Title", buttonTitleString);
+        buttonTitleText = EditorGUILayout.ObjectField("Button Title Text", buttonTitleText, typeof(TMP_Text), true) as TMP_Text;
+
+
+        if (GUI.changed)
+        {
+            informationText.text = informationString;
+            EditorUtility.SetDirty(informationText);
+            titleText.text = titleString;
+            EditorUtility.SetDirty(titleText);
+            buttonTitleText.text = buttonTitleString;
+            EditorUtility.SetDirty(buttonTitleText);
+            EditorUtility.SetDirty(tutorialMapObject);
+            EditorUtility.SetDirty(maskTransform);
+            EditorUtility.SetDirty(panelTransform);
+            EditorUtility.SetDirty(triangleTransform);
+        }
+        if (GUILayout.Button("Save Tutorial Preset"))
+        {
+            SaveTutorial();
+        }
+    }
+    private void SaveTutorial()
+    {
+        SetMaskRect();
+        SetPanelRect();
+        SetTitleText();
+        SetInformationText();
+        SetButtonTitle();
+        SetTriangle();
+        SetMapRect();
+    }
+    void SetMapRect()
+    {
+        tutorialMapObject.mapButtonPosition = tutorialMapButtonTransform.anchoredPosition;
+        tutorialMapObject.mapButtonWidthAndHeight = tutorialMapButtonTransform.sizeDelta;
+        tutorialMapObject.mapButtonAnchorMin = tutorialMapButtonTransform.anchorMin;
+        tutorialMapObject.mapButtonAnchorMax = tutorialMapButtonTransform.anchorMax;
+        tutorialMapObject.mapButtonPivot = tutorialMapButtonTransform.pivot;
+    }
+
+    void SetMaskRect()
+    {
+        tutorialMapObject.maskPosition = maskTransform.anchoredPosition;
+        tutorialMapObject.maskWidthAndHeight = maskTransform.sizeDelta;
+        tutorialMapObject.maskAnchorMin = maskTransform.anchorMin;
+        tutorialMapObject.maskAnchorMax = maskTransform.anchorMax;
+        tutorialMapObject.maskPivot = maskTransform.pivot;
+    }
+    void SetPanelRect()
+    {
+        tutorialMapObject.panelPosition = panelTransform.anchoredPosition;
+        tutorialMapObject.panelWidthAndHeight = panelTransform.sizeDelta;
+        tutorialMapObject.panelAnchorMin = panelTransform.anchorMin;
+        tutorialMapObject.panelAnchorMax = panelTransform.anchorMax;
+        tutorialMapObject.panelPivot = panelTransform.pivot;
+    }
+    void SetTriangle()
+    {
+        tutorialMapObject.trianglePosition = triangleTransform.anchoredPosition;
+        tutorialMapObject.triangleWidthAndHeight = triangleTransform.sizeDelta;
+        tutorialMapObject.triangleAnchorMin = panelTransform.anchorMin;
+        tutorialMapObject.triangleAnchorMax = triangleTransform.anchorMax;
+        tutorialMapObject.trianglePivot = triangleTransform.pivot;
+        tutorialMapObject.triangleRotation = triangleTransform.localEulerAngles;
+
+    }
+    void SetTitleText()
+    {
+        tutorialMapObject.titleTextPosition = titleText.rectTransform.anchoredPosition;
+        tutorialMapObject.titleTextWidthAndHeight = titleText.rectTransform.sizeDelta;
+        tutorialMapObject.titleTextAnchorMin = titleText.rectTransform.anchorMin;
+        tutorialMapObject.titleTextAnchorMax = titleText.rectTransform.anchorMax;
+        tutorialMapObject.titleTextPivot = titleText.rectTransform.pivot;
+        tutorialMapObject.titleText = titleText.text;
+        tutorialMapObject.titleFontSize = titleText.fontSize;
+    }
+    void SetInformationText()
+    {
+        tutorialMapObject.informationTextPosition = informationText.rectTransform.anchoredPosition;
+        tutorialMapObject.informationTextWidthAndHeight = informationText.rectTransform.sizeDelta;
+        tutorialMapObject.informationTextAnchorMin = informationText.rectTransform.anchorMin;
+        tutorialMapObject.informationTextAnchorMax = informationText.rectTransform.anchorMax;
+        tutorialMapObject.informationTextPivot = informationText.rectTransform.pivot;
+        tutorialMapObject.informationText = informationText.text;
+        tutorialMapObject.informationFontSize = informationText.fontSize;
+    }
+    void SetButtonTitle()
+    {
+        tutorialMapObject.buttonTitle = buttonTitleText.text;
+    }
+}
---
+++ Editor/TutorialEditor.cs
@@ -0,0 +1,107 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TutorialEditor : EditorWindow
+{
+    string informationString;
+    string titleString;
+    TutorialScriptableObjects tutorialObject;
+    RectTransform maskTransform;
+    RectTransform panelTransform;
+    RectTransform triangleTransform;
+    [SerializeField]
+    TMP_Text informationText;
+    [SerializeField]
+    TMP_Text titleText;
+
+    [MenuItem("Tools/TutorialEditor")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(TutorialEditor));
+    }
+    private void OnGUI()
+    {
+        GUILayout.Label("Save Tutorial Preset", EditorStyles.boldLabel);
+        tutorialObject = EditorGUILayout.ObjectField("Tutorial Scriptable Object", tutorialObject, typeof(TutorialScriptableObjects), false) as TutorialScriptableObjects;
+        maskTransform = EditorGUILayout.ObjectField("Mask Rect Transform", maskTransform, typeof(RectTransform), true) as RectTransform;
+        panelTransform = EditorGUILayout.ObjectField("Panel Rect Transform", panelTransform, typeof(RectTransform), true) as RectTransform;
+        triangleTransform = EditorGUILayout.ObjectField("Triangle Rect Transform", triangleTransform, typeof(RectTransform), true) as RectTransform;
+        titleString = EditorGUILayout.TextField("Title", titleString);
+        titleText = EditorGUILayout.ObjectField("Title Text", titleText, typeof(TMP_Text), true) as TMP_Text;
+        informationString = EditorGUILayout.TextField("Information", informationString);
+        informationText = EditorGUILayout.ObjectField("Information Text", informationText, typeof(TMP_Text), true) as TMP_Text;
+
+
+        if (GUI.changed)
+        {
+            informationText.text = informationString;
+            EditorUtility.SetDirty(informationText);
+            titleText.text = titleString;
+            EditorUtility.SetDirty(titleText);
+            EditorUtility.SetDirty(tutorialObject);
+            EditorUtility.SetDirty(maskTransform);
+            EditorUtility.SetDirty(panelTransform);
+            EditorUtility.SetDirty(triangleTransform);
+        }
+        if (GUILayout.Button("Save Tutorial Preset"))
+        {
+            SaveTutorial();
+        }
+    }
+    private void SaveTutorial()
+    {
+        SetMaskRect();
+        SetPanelRect();
+        SetTitleText();
+        SetInformationText();
+        SetTringle();
+    }
+    void SetMaskRect()
+    {
+        tutorialObject.maskPosition = maskTransform.anchoredPosition;
+        tutorialObject.maskWidthAndHeight = maskTransform.sizeDelta;
+        tutorialObject.maskAnchorMin = maskTransform.anchorMin;
+        tutorialObject.maskAnchorMax = maskTransform.anchorMax;
+        tutorialObject.maskPivot = maskTransform.pivot;
+    }
+    void SetPanelRect()
+    {
+        tutorialObject.panelPosition = panelTransform.anchoredPosition;
+        tutorialObject.panelWidthAndHeight = panelTransform.sizeDelta;
+        tutorialObject.panelAnchorMin = panelTransform.anchorMin;
+        tutorialObject.panelAnchorMax = panelTransform.anchorMax;
+        tutorialObject.panelPivot = panelTransform.pivot;
+    }
+    void SetTringle()
+    {
+        tutorialObject.trianglePosition = triangleTransform.anchoredPosition;
+        tutorialObject.triangleWidthAndHeight = triangleTransform.sizeDelta;
+        tutorialObject.triangleAnchorMin = panelTransform.anchorMin;
+        tutorialObject.triangleAnchorMax = triangleTransform.anchorMax;
+        tutorialObject.trianglePivot = triangleTransform.pivot;
+        tutorialObject.triangleRotation = triangleTransform.localEulerAngles;
+
+    }
+    void SetTitleText()
+    {
+        tutorialObject.titleTextPosition = titleText.rectTransform.anchoredPosition;
+        tutorialObject.titleTextWidthAndHeight = titleText.rectTransform.sizeDelta;
+        tutorialObject.titleTextAnchorMin = titleText.rectTransform.anchorMin;
+        tutorialObject.titleTextAnchorMax = titleText.rectTransform.anchorMax;
+        tutorialObject.titleTextPivot = titleText.rectTransform.pivot;
+        tutorialObject.titleText = titleText.text;
+        tutorialObject.titleFontSize = titleText.fontSize;
+    }
+    void SetInformationText()
+    {
+        tutorialObject.informationTextPosition = informationText.rectTransform.anchoredPosition;
+        tutorialObject.informationTextWidthAndHeight = informationText.rectTransform.sizeDelta;
+        tutorialObject.informationTextAnchorMin = informationText.rectTransform.anchorMin;
+        tutorialObject.informationTextAnchorMax = informationText.rectTransform.anchorMax;
+        tutorialObject.informationTextPivot = informationText.rectTransform.pivot;
+        tutorialObject.informationText = informationText.text;
+        tutorialObject.informationFontSize = informationText.fontSize;
+    }
+}
